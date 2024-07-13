d12e7fd1370c9f1189a70f3c2b48106ba36a1ee5
sbx_user1051 <sbx_user1051@169.254.6.109>
2024-07-13 21:54:27 +0000

    Sat Jul 13 21:54:27 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,8 +1,16 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    
-    if x < 0 or x > 100 or y < 0 or y > 100:
+
+    # Check if both values are integers
+    if not isinstance(x, int) or not isinstance(y, int):
+        raise ValueError("Both values must be integers")
+
+    # Check if both values are between 0 and 100
+    elif x < 0 or x > 100 or y < 0 or y > 100:
         raise ValueError("Both values must be between 0 and 100")
+    
+    # Return the sum of x and y
     else:
         return x + y
+
 
