67a39bea9c831558554dcc50f9ca6682caaf79d7
sbx_user1051 <sbx_user1051@169.254.24.109>
2024-07-13 22:00:14 +0000

    Sat Jul 13 22:00:14 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,5 +3,6 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    return "I'm going to ignore the input and return this sentence instead."
+    return "Hello, World!"
+
 
